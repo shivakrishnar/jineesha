6d68986c4345c253da1bf081a960d243191130ec
Cuong Lai <Cuong.Lai@asuresoftware.com>
2019-07-24 09:37:45 -0400

    fix: use correct global admin role name

--- services/api/models/Role.ts
+++ services/api/models/Role.ts
@@ -1,7 +1,7 @@
 export enum Role {
     hrManager = 'hr.persona.manager',
     asureAdmin = 'asure-admin',
-    globalAdmin = 'global-admin',
+    globalAdmin = 'global.admin',
     serviceBureauAdmin = 'service.bureau.admin',
     superAdmin = 'super.admin',
     hrAdmin = 'hr.persona.admin',
