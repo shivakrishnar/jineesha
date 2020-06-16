30f3b135ec0b3fec51a041bb75fc55a7cd206e94
Kiran <kirankumar.b@accionlabs.com>
2020-06-16 22:20:22 +0530

    test: add unit tests for the URL structure changes in candidate export

--- services/integrations/applicant-tracking/unit-tests/applicant-tracking.handler.spec.ts
+++ services/integrations/applicant-tracking/unit-tests/applicant-tracking.handler.spec.ts
@@ -23,46 +23,50 @@ describe('Configuring Webhook in JazzHR', () => {
         });
     });
 
+    test('returns 400 error.Not authorized when query string parameter id is missing', () => {
+        const handlerInput: IGatewayEventInput = {
+            securityContext: null,
+            event: jzMockCallbackData.jzUnsupportedQueryParam,
+            requestBody: jzMockCallbackData.jzRequestBody,
+        };
+
+        return eventCallbackDelegate(handlerInput)
+            .catch((error: any) => {
+                expect(error).toBeInstanceOf(ErrorMessage);
+                expect(error.statusCode).toEqual(400);
+                expect(error.code).toEqual(30);
+                expect(error.message).toEqual('The provided request object was not valid for the requested operation.');
+                const developerMessage = 'Unsupported query parameter(s) supplied';
+                expect(error.developerMessage).toEqual(developerMessage);
+                const moreInfo = 'Available query parameters: id. See documentation for usage.';
+                expect(error.moreInfo).toEqual(moreInfo);
+            });
+    });
+
+    test('returns 400 error.Not authorized when delimiter is missing in query string value format', () => {
+        const handlerInput: IGatewayEventInput = {
+            securityContext: null,
+            event: jzMockCallbackData.jzUnsupportedQueryValueFormat,
+            requestBody: jzMockCallbackData.jzRequestBody,
+        };
+
+        return eventCallbackDelegate(handlerInput)
+            .catch((error: any) => {
+                expect(error).toBeInstanceOf(ErrorMessage);
+                expect(error.statusCode).toEqual(400);
+                expect(error.code).toEqual(30);
+                expect(error.message).toEqual('The provided request object was not valid for the requested operation.');
+                const developerMessage = 'Unsupported value';
+                expect(error.developerMessage).toContain(developerMessage);
+                const moreInfo = 'Value format for id: tenantId_companyId.';
+                expect(error.moreInfo).toEqual(moreInfo);
+            });
+    });
+
     test('returns 401 error.Not authorized when request is without JazzHR special header', () => {
         const handlerInput: IGatewayEventInput = {
             securityContext: null,
             event: jzMockCallbackData.jzNoSpecialHeader,
-            requestBody: jzMockCallbackData.jzRequestBody,
-        };
-
-        return eventCallbackDelegate(handlerInput)
-            .catch((error: any) => {
-                expect(error).toBeInstanceOf(ErrorMessage);
-                expect(error.statusCode).toEqual(401);
-                expect(error.code).toEqual(11);
-                expect(error.message).toEqual('User is not authorized.');
-                const developerMessage = 'The user does not have authorization to use this endpoint.';
-                expect(error.developerMessage).toEqual(developerMessage);
-            });
-    });
-
-    test('returns 401 error.Not authorized when tenantId missing in event path parameters', () => {
-        const handlerInput: IGatewayEventInput = {
-            securityContext: null,
-            event: jzMockCallbackData.jzNoTenantId,
-            requestBody: jzMockCallbackData.jzRequestBody,
-        };
-
-        return eventCallbackDelegate(handlerInput)
-            .catch((error: any) => {
-                expect(error).toBeInstanceOf(ErrorMessage);
-                expect(error.statusCode).toEqual(401);
-                expect(error.code).toEqual(11);
-                expect(error.message).toEqual('User is not authorized.');
-                const developerMessage = 'The user does not have authorization to use this endpoint.';
-                expect(error.developerMessage).toEqual(developerMessage);
-            });
-    });
-
-    test('returns 401 error.Not authorized when companyId missing in event path parameters', () => {
-        const handlerInput: IGatewayEventInput = {
-            securityContext: null,
-            event: jzMockCallbackData.jzNoCompanyId,
             requestBody: jzMockCallbackData.jzRequestBody,
         };
 
