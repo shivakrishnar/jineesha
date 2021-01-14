03971972d6581e9c7577cab7fb0bfea4cd936c74
Cuong Lai <Cuong.Lai@asuresoftware.com>
2021-01-14 10:05:27 -0500

    feat: update ssm scripts with pricing data

--- scripts/esignature-config-setup-prod.sh
+++ scripts/esignature-config-setup-prod.sh
@@ -2,4 +2,6 @@ old_aws_config_cli_follow_urlparam_value=$(aws configure get cli_follow_urlparam
 aws configure set cli_follow_urlparam false
 # TODO: update the date upon release (MJ-6856)
 aws ssm put-parameter --name "/hr/esignature/simplesign/legacyClientCutOffDate" --value "10/22/2020" --type String --overwrite
+aws ssm put-parameter --name "/hr/esignature/simplesign/directClientPricingData" --value "{\"monthlyCost\":\"10.00\",\"costPerRequest\":\"1.00\"}" --type String --overwrite
+aws ssm put-parameter --name "/hr/esignature/simplesign/indirectClientPricingData" --value "{\"monthlyCost\":\"6.00\",\"costPerRequest\":\"0.55\"}" --type String --overwrite
 aws configure set cli_follow_urlparam $old_aws_config_cli_follow_urlparam_value
