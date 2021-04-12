c245956692efb9c8e0cb6cf023fd13f6b136ea8b
Cuong Lai <Cuong.Lai@asuresoftware.com>
2021-04-12 14:22:27 -0400

    use module.export in cancel mock

--- services/integrations/__mocks__/mockFunctions/helloSignCancelSignatureRequest.js
+++ services/integrations/__mocks__/mockFunctions/helloSignCancelSignatureRequest.js
@@ -1,7 +1,11 @@
+'use strict';
+
 class mockCancel {
     static cancel = jest.fn(() => {
         return {};
     });
 }
 
-export const cancelMock = mockCancel.cancel;
+module.exports = {
+    cancelMock: mockCancel.cancel,
+};
