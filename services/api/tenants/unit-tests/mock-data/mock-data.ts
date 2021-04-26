2cf2db9f7d23902ef49f56aa9b995d5294461088
Cuong Lai <Cuong.Lai@asuresoftware.com>
2021-04-26 10:13:47 -0400

    test: add tests for company update patch endpoint

---
+++ services/api/tenants/unit-tests/mock-data/mock-data.ts
@@ -0,0 +1,93 @@
+import { EsignatureAppConfiguration } from '../../../../remote-services/integrations.service';
+import { PatchInstruction, PatchOperation } from '../../src/patchInstruction';
+
+export const oldTenantId = '1234';
+export const newTenantId = '5678';
+export const companyCode = '1';
+
+export const platformIntegrationPatchInstructions: PatchInstruction[] = [
+    {
+        op: PatchOperation.Test,
+        path: '/platform/integration',
+        value: newTenantId,
+    },
+];
+
+export const testPatchInstruction: PatchInstruction = {
+    op: PatchOperation.Test,
+    path: '/test',
+};
+
+export const oldIntegrationConfiguration: EsignatureAppConfiguration = {
+    id: '1234',
+    integrationId: '1234',
+    tenantId: '1234',
+    clientId: 1234,
+    companyId: 1234,
+    companyName: 'Old Company',
+    integrationDetails: {
+        domainName: 'old.test.com',
+        eSignatureAppClientId: '1234',
+        enabled: true,
+    },
+    createAt: '2019-09-26T03:14:23.141Z',
+    createdBy: {
+        id: '1234',
+        username: 'olduser',
+    },
+};
+
+export const newIntegrationConfiguration: EsignatureAppConfiguration = {
+    id: '5678',
+    integrationId: '5678',
+    tenantId: '5678',
+    clientId: 5678,
+    companyId: 5678,
+    companyName: 'New Company',
+    integrationDetails: {
+        domainName: 'new.test.com',
+        eSignatureAppClientId: '5678',
+        enabled: true,
+    },
+    createAt: '2019-09-26T03:14:23.141Z',
+    createdBy: {
+        id: '5678',
+        username: 'newuser',
+    },
+};
+
+export const oldHsApp = JSON.stringify({
+    api_app: {
+        name: 'oldapp',
+        domain: 'old.test.com',
+    },
+});
+
+export const newHsApp = JSON.stringify({
+    api_app: {
+        name: 'newapp',
+        domain: 'new.test.com',
+    },
+});
+
+export const oldCompanyDBResponse = {
+    recordset: [
+        {
+            ID: 1234,
+            PRIntegration_ClientID: '1234',
+        },
+    ],
+    output: {},
+    rowsAffected: [1],
+};
+
+export const newCompanyDBResponse = {
+    recordset: [
+        {
+            ID: 5678,
+            PRIntegration_ClientID: '5678',
+        },
+    ],
+    output: {},
+    rowsAffected: [1],
+};
