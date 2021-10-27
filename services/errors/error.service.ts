b15f9aba4ca9bb3644b8a38aeb50f6aa7e05b4e6
Pam Mercier <pam.mercier@asuresoftware.com>
2021-10-27 14:37:52 -0400

    refactor: hoist functions
    hoist functions to be defined before usage

--- services/errors/error.service.ts
+++ services/errors/error.service.ts
@@ -1,31 +1,6 @@
 import { ErrorMessage } from './errorMessage';
 
 /**
- * Returns a properly formatted ErrorMessage object with messaging for the
- * code that was requested. If no matching code is found it will default to the
- * messaging for a 500 error.
- *
- * @param {number} code
- */
-export function getErrorResponse(code: number): ErrorMessage {
-    return findErrorMessage(code);
-}
-
-export function notAuthenticated(): ErrorMessage {
-    return findErrorMessage(10);
-}
-
-export function notAuthorized(role: string): ErrorMessage {
-    const errorMessage = findErrorMessage(20);
-    errorMessage.setDeveloperMessage(`The principal does not have the required role (${role}).`);
-    return errorMessage;
-}
-
-export function notFound(): ErrorMessage {
-    return findErrorMessage(50);
-}
-
-/**
  * Private function which locates an error message based on the code that is
  * passed in. If a message with the code is not found then the default message
  * with an status code of 500 is returned.
@@ -138,4 +113,29 @@ function errorMessages(): ErrorMessage[] {
       "moreInfo": ""
     }
   ]`);
+}
+
+/**
+ * Returns a properly formatted ErrorMessage object with messaging for the
+ * code that was requested. If no matching code is found it will default to the
+ * messaging for a 500 error.
+ *
+ * @param {number} code
+ */
+export function getErrorResponse(code: number): ErrorMessage {
+    return findErrorMessage(code);
+}
+
+export function notAuthenticated(): ErrorMessage {
+    return findErrorMessage(10);
+}
+
+export function notAuthorized(role: string): ErrorMessage {
+    const errorMessage = findErrorMessage(20);
+    errorMessage.setDeveloperMessage(`The principal does not have the required role (${role}).`);
+    return errorMessage;
+}
+
+export function notFound(): ErrorMessage {
+    return findErrorMessage(50);
 }
