7f0805af12730713dc0d24c635042d0bd6f3fb5b
Mohamed Ammar <mohamed.ammar@asuresoftware.com>
2022-01-21 09:14:21 -0500

    test: Create unit & integration tests for employeeClass endpoint

---
+++ integration-tests/tenants/get.classes-by-employee-id.int-spec.ts
@@ -0,0 +1,160 @@
+import * as fs from 'fs';
+import * as request from 'supertest';
+import * as utils from '../utils';
+
+const configs = utils.getConfig();
+const baseUri = `${configs.nonProxiedApiDomain}/internal`;
+let adminAccessToken: string;
+let employeeAccessToken: string;
+
+const errorMessageSchema = JSON.parse(fs.readFileSync('services/api/models/ErrorMessage.json').toString());
+const paginatedResultSchema = JSON.parse(fs.readFileSync('services/integrations/models/PaginatedResult.json').toString());
+const classSchema = JSON.parse(fs.readFileSync('services/api/models/Class.json').toString());
+const classesSchema = JSON.parse(fs.readFileSync('services/api/models/Classes.json').toString());
+
+const schemas = [errorMessageSchema, paginatedResultSchema, classSchema, classesSchema];
+
+const enum schemaNames {
+    ErrorMessage = 'ErrorMessage',
+    PaginatedResult = 'PaginatedResult',
+    Classes = 'Classes',
+    Class = 'Class',
+}
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+describe('get classes by employee id as an admin user', () => {
+    beforeAll(async (done) => {
+        try {
+            adminAccessToken = await utils.getAccessToken(configs.sbAdminUser.username, configs.sbAdminUser.password);
+            done();
+        } catch (error) {
+            done.fail(error);       
+        }
+    });
+
+    test('must return a 401 if a token is not provided', (done) => {
+        const uri = `/tenants/${configs.classes.tenantId}/companies/${configs.classes.companyId}/employees/${
+            configs.classes.employeeId
+        }/classes`;
+        request(baseUri)
+            .get(uri)
+            .expect(utils.corsAssertions(configs.corsAllowedHeaderList))
+            .expect(401)
+            .end((error, response) => {
+                utils.testResponse(error, response, done, () => {
+                    return utils.assertJson(schemas, schemaNames.ErrorMessage, response.body);
+                });
+            });
+    });
+
+    [{ tenantId: configs.invalidTenantId }, { companyId: configs.invalidCompanyId }, { employeeId: configs.invalidEmployeeId }].forEach(
+        (endpoint) => {
+            test(`must return a 400 if ${Object.keys(endpoint)} is invalid`, (done) => {
+                request(baseUri)
+                    .get(
+                        `/tenants/${endpoint.tenantId || configs.classes.tenantId}/companies/${endpoint.companyId ||
+                            configs.classes.companyId}/employees/${endpoint.employeeId || configs.classes.employeeId}/classes`,
+                    )
+                    .set('Authorization', `Bearer ${adminAccessToken}`)
+                    .expect(utils.corsAssertions(configs.corsAllowedHeaderList))
+                    .expect(400)
+                    .end((error, response) => {
+                        utils.testResponse(error, response, done, () => {
+                            return utils.assertJson(schemas, schemaNames.ErrorMessage, response.body);
+                        });
+                    });
+            });
+        },
+    );
+
+    [
+        { tenantId: configs.nonExistentTenantId },
+        { companyId: configs.nonExistentCompanyId },
+        { employeeId: configs.nonExistentEmployeeId },
+    ].forEach((endpoint) => {
+        test(`must return a 404 if ${Object.keys(endpoint)} is not found`, (done) => {
+            request(baseUri)
+                .get(
+                    `/tenants/${endpoint.tenantId || configs.classes.tenantId}/companies/${endpoint.companyId ||
+                        configs.classes.companyId}/employees/${endpoint.employeeId || configs.classes.employeeId}/classes`,
+                )
+                .set('Authorization', `Bearer ${adminAccessToken}`)
+                .expect(utils.corsAssertions(configs.corsAllowedHeaderList))
+                .expect(404)
+                .end((error, response) => {
+                    utils.testResponse(error, response, done, () => {
+                        return utils.assertJson(schemas, schemaNames.ErrorMessage, response.body);
+                    });
+                });
+        });
+    });
+
+    test('must return a 200 if classes exists', (done) => {
+        const uri = `/tenants/${configs.classes.tenantId}/companies/${configs.classes.companyId}/employees/${
+            configs.classes.employeeId
+        }/classes`;
+        request(baseUri)
+            .get(uri)
+            .set('Authorization', `Bearer ${adminAccessToken}`)
+            .expect(utils.corsAssertions(configs.corsAllowedHeaderList))
+            .expect(200)
+            .end((error, response) => {
+                utils.testResponse(error, response, done, () => {
+                    return utils.assertJson(schemas, schemaNames.PaginatedResult, response.body);
+                });
+                utils.testResponse(error, response, done, () => {
+                    return utils.assertJson(schemas, schemaNames.Classes, response.body.results);
+                });
+                utils.testResponse(error, response, done, () => {
+                    return utils.assertJson(schemas, schemaNames.Class, response.body.results[0]);
+                });
+            });
+    });
+});
+
+describe('get classes by employee Id as an employee user', () => {
+    beforeAll(async (done) => {
+        try {
+            employeeAccessToken = await utils.getAccessToken(configs.employeeUser.review.username, configs.employeeUser.review.password);
+            done();
+        } catch (error) {
+            done.fail(error);
+        }
+    });
+
+    test('must return 401 error if employee level user is not tied to the requested employee', async (done) => {
+        const uri = `/tenants/${configs.classes.tenantId}/companies/${configs.classes.companyId}/employees/${
+            configs.classes.userAccessTest.unrightfulEmployee}/classes`
+
+        request(baseUri)
+            .get(uri)
+            .set('Authorization', `Bearer ${employeeAccessToken}`)
+            .expect(utils.corsAssertions(configs.corsAllowedHeaderList))
+            .expect(401)
+            .end((error, response) => {
+                utils.testResponse(error, response, done, () => {
+                     return utils.assertJson(schemas, schemaNames.ErrorMessage, response.body);
+                });
+            });
+    });
+
+    test('must return a 200 if employee level user is tied to the requested employee', async (done) => {
+        const uri = `/tenants/${configs.classes.tenantId}/companies/${configs.classes.companyId}/employees/${
+            configs.classes.userAccessTest.rightfulEmployee}/classes`;
+        request(baseUri)
+            .get(uri)
+            .set('Authorization', `Bearer ${employeeAccessToken}`)
+            .expect(utils.corsAssertions(configs.corsAllowedHeaderList))
+            .expect(200)
+            .end((error, response) => {
+                utils.testResponse(error, response, done, () => {
+                    return utils.assertJsonOrThrow(schemas, classesSchema, response.body.results);
+                });
+                utils.testResponse(error, response, done, () => {
+                    return utils.assertJson(schemas, schemaNames.PaginatedResult, response.body);
+                });
+            });
+    });
+});
+
---
+++ services/api/tenants/unit-tests/ListClassesByEmployeeId.spec.ts
@@ -0,0 +1,96 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import 'reflect-metadata'; // required by asure.auth dependency
+import * as utilService from '../../../util.service';
+import * as errorService from '../../../errors/error.service';
+import * as mockData from './mock-data';
+import * as service from '../src/employee.service';
+import { setup } from '../../../unit-test-mocks/mock';
+import { PaginatedResult } from '../../../pagination/paginatedResult';
+
+const errorMessage = {
+    statusCode: 500,
+    code: 0,
+    message: 'Unexpected error occurred.',
+    developerMessage: 'Something happened on the server and we have no idea what. Blame the architect.',
+    moreInfo: '',
+};
+
+describe('listClassesByEmployeeId Service', () => {
+    beforeEach(() => {
+        setup();
+    })
+
+    test('returns Classes', () => {
+        (utilService as any).validateEmployeeWithCompany = jest.fn(() => {
+            return Promise.resolve();
+        });
+
+        (utilService as any).invokeInternalService = jest.fn(() => {
+            return Promise.resolve(mockData.upcomingClassesResult);
+        });
+
+        return service
+            .listClassesByEmployeeId(
+                mockData.tenantId,
+                mockData.companyId,
+                mockData.employeeId,
+                undefined,
+                mockData.domainName,
+                mockData.path,
+            )
+            .then((classes) => {
+                console.log(mockData.upcomingClassesResponse[0])
+                console.log(classes.results[0])
+                expect(classes).toBeInstanceOf(PaginatedResult);
+                expect(classes.results.length).toBe(mockData.upcomingClassesResponse.length);
+                expect(classes.results[0]).toEqual(mockData.upcomingClassesResponse[0]);
+                expect(classes.results[1]).toEqual(mockData.upcomingClassesResponse[1]);
+                expect(classes.results[2]).toEqual(mockData.upcomingClassesResponse[2]);
+                expect(classes.results[3]).toEqual(mockData.upcomingClassesResponse[3]);
+            });
+    });
+
+    test('returns empty resources', () => {
+        (utilService as any).validateEmployeeWithCompany = jest.fn(() => {
+            return Promise.resolve();
+        });
+
+        (utilService as any).invokeInternalService = jest.fn(() => {
+            return Promise.resolve(mockData.emptyUpcomingClassesResult);
+        });
+
+        return service
+            .listClassesByEmployeeId(
+                mockData.tenantId,
+                mockData.companyId,
+                mockData.employeeId,
+                undefined,
+                mockData.domainName,
+                mockData.path,
+            )
+            .then((Classes) => {
+                expect(Classes).toBeInstanceOf(PaginatedResult);
+                expect(Classes.results.length).toBe(0);
+                expect(Classes.results).toEqual([]);
+            });
+    });
+
+    test('should return error', () => {
+        (utilService as any).validateEmployeeWithCompany = jest.fn(() => {
+            throw errorService.getErrorResponse(0);
+        });
+
+        return service
+            .listClassesByEmployeeId(
+                mockData.tenantId,
+                mockData.companyId,
+                mockData.employeeId,
+                undefined,
+                mockData.domainName,
+                mockData.path,
+            )
+            .catch((error) => {
+                expect(error).toEqual(errorMessage);
+            });
+    });
+});
