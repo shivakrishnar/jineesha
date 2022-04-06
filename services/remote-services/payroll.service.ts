a97befa0e019f896c9a2f6d4a9f51adb399fc948
Cuong Lai <Cuong.Lai@asuresoftware.com>
2022-04-06 17:04:00 -0400

    fix: retrieve company time off categories and update summary logic

--- services/remote-services/payroll.service.ts
+++ services/remote-services/payroll.service.ts
@@ -4,6 +4,7 @@ import * as configService from '../config.service';
 import { IEvolutionKey } from '../api/models/IEvolutionKey';
 
 const baseUrl = `${configService.getApiDomain()}/v3/api/bureaus`;
+const unproxiedBaseUrl = `${configService.getApiDomain()}/v3/api/bureau`;
 
 export async function getEvolutionEarningAndDeduction(tenantName: string, evolutionKeys: IEvolutionKey, token: string): Promise<any> {
     console.info('payrollService.getEvolutionEarningAndDeduction');
@@ -121,6 +122,27 @@ export async function getEvolutionTimeOffSummariesByEmployeeId(
     const { clientId, companyId, employeeId } = evolutionKeys;
 
     const apiUrl = `${baseUrl}/${tenantName}/clients/${clientId}/companies/${companyId}/employees/${employeeId}/time-off-summaries`;
+    try {
+        return await request.get({
+            url: encodeURI(apiUrl),
+            headers: { Authorization: `Bearer ${token}` },
+            json: true,
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export async function getEvolutionCompanyTimeOffCategoriesByCompanyId(
+    tenantName: string,
+    evolutionKeys: IEvolutionKey,
+    token: string,
+): Promise<any> {
+    console.info('payrollService.getEvolutionCompanyTimeOffCategoriesByCompanyId');
+
+    const { clientId, companyId } = evolutionKeys;
+
+    const apiUrl = `${unproxiedBaseUrl}/${tenantName}/clients/${clientId}/companies/${companyId}/companytimeoffaccruals`;
     try {
         return await request.get({
             url: encodeURI(apiUrl),
