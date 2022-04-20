74cde790747612dfd08c27497c8fac3e4c81acd4
Cuong Lai <Cuong.Lai@asuresoftware.com>
2022-04-20 15:14:37 -0400

    feat: add functionality to listCompanies endpoint
    - Added two new properties to response object: code and createDate
    - Updated the endpoint to accept a search query param that will filter
    the database results based on the company name and code.

--- services/queries/companies/listCompanies.sql
+++ services/queries/companies/listCompanies.sql
@@ -1,12 +1,19 @@
+declare @_search as nvarchar(max) = '%' + @search + '%';
 
 -- Get total count for pagination
 select
     count(*) as totalCount
 from
     dbo.Company
+where
+    concat(CompanyName, PRIntegrationCompanyCode) like @_search
 
 select
     ID,
-    CompanyName
+    CompanyName,
+    PRIntegrationCompanyCode,
+    CreateDate
 from
     dbo.Company
+where
+    concat(CompanyName, PRIntegrationCompanyCode) like @_search
