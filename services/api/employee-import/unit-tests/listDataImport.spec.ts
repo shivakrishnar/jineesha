dd3563a846397bee76e7e8b676788a5a1bffa089
johnn <johnnathan.almeida.ctr@asuresoftware.com>
2024-01-17 18:04:24 -0300

    Was wrote some unit tests and reduced branch coverage metrics

--- services/api/employee-import/unit-tests/listDataImport.spec.ts
+++ services/api/employee-import/unit-tests/listDataImport.spec.ts
@@ -5,6 +5,7 @@ import * as employeeImportService from '../src/EmployeeImport.Service';
 import * as mockData from './mock-data';
 import { setup } from '../../../unit-test-mocks/mock';
 import { PaginatedResult } from '../../../pagination/paginatedResult';
+import { ErrorMessage } from '../../../errors/errorMessage';
 
 describe('dataimporttype.service.list', () => {
     beforeEach(() => {
@@ -14,15 +15,25 @@ describe('dataimporttype.service.list', () => {
     test('returns the list of all data import types', async () => {
         (utilService as any).invokeInternalService = jest.fn((transaction, payload) => {
             if (payload.queryName === 'listDataImportTypes') {
-				return Promise.resolve(mockData.dataImportTypes);
-			}
+                return Promise.resolve(mockData.dataImportTypes);
+            }
         });
 
-        await employeeImportService
-            .listDataImportTypes(mockData.devTenantId)
-            .then((response) => {
-                expect(response).toEqual(mockData.dataImportTypeResponse.recordset);
-            });
+        await employeeImportService.listDataImportTypes(mockData.devTenantId).then((response) => {
+            expect(response).toEqual(mockData.dataImportTypeResponse.recordset);
+        });
+    });
+
+    test('returns empty for wrong tenant id', async () => {
+        (utilService as any).invokeInternalService = jest.fn((transaction, payload) => {
+            if (payload.queryName === 'listDataImportTypes') {
+                return Promise.resolve(mockData.dataImportTypesEmpty);
+            }
+        });
+
+        await employeeImportService.listDataImportTypes('123456789').then((response) => {
+            expect(response).toEqual([]);
+        });
     });
 });
 
@@ -34,16 +45,46 @@ describe('dataimport.service.list', () => {
     test('returns the list of data imports', async () => {
         (utilService as any).invokeInternalService = jest.fn((transaction, payload) => {
             if (payload.queryName === 'listDataImportByCompany') {
-				return Promise.resolve(mockData.dataImports);
-			}
+                return Promise.resolve(mockData.dataImports);
+            }
         });
 
         await employeeImportService
-            .listDataImports(mockData.devTenantId, mockData.CompanyId, "", null, mockData.domainName, mockData.path)
+            .listDataImports(mockData.devTenantId, mockData.CompanyId, '', null, mockData.domainName, mockData.path)
             .then((response) => {
                 expect(response).toBeInstanceOf(PaginatedResult);
                 expect(response.results.length).toBe(mockData.dataImportsResponse.results.length);
                 expect(response.results).toEqual(mockData.dataImportsResponse.results);
+            });
+    });
+
+    test('returns the list of data imports by data import type id', async () => {
+        (utilService as any).invokeInternalService = jest.fn((transaction, payload) => {
+            if (payload.queryName === 'listDataImportByCompanyAndDataImportType') {
+                return Promise.resolve(mockData.dataImportsByDataImportTypeId);
+            }
+        });
+
+        await employeeImportService
+            .listDataImports(mockData.devTenantId, mockData.CompanyId, mockData.DataImportTypeId, null, mockData.domainName, mockData.path)
+            .then((response) => {
+                expect(response).toBeInstanceOf(PaginatedResult);
+                expect(response.results.length).toBe(mockData.dataImportsByDataImportTypeResponse.results.length);
+                expect(response.results).toEqual(mockData.dataImportsByDataImportTypeResponse.results);
+            });
+    });
+
+    test('returns undefined when the company has no imports', async () => {
+        (utilService as any).invokeInternalService = jest.fn((transaction, payload) => {
+            if (payload.queryName === 'listDataImportByCompanyAndDataImportType') {
+                return Promise.resolve(mockData.dataImportsEmpty);
+            }
+        });
+
+        await employeeImportService
+            .listDataImports(mockData.devTenantId, '123456789', mockData.DataImportTypeId, null, mockData.domainName, mockData.path)
+            .then((response) => {
+                expect(response).toBe(undefined);
             });
     });
 });
@@ -56,16 +97,160 @@ describe('dataimporteventdetails.service.list', () => {
     test('returns the list of data import event details', async () => {
         (utilService as any).invokeInternalService = jest.fn((transaction, payload) => {
             if (payload.queryName === 'listDataImportEventDetail') {
-				return Promise.resolve(mockData.dataImportEventDetails);
-			}
+                return Promise.resolve(mockData.dataImportEventDetails);
+            }
         });
 
         await employeeImportService
-            .listDataImportEventDetails(mockData.devTenantId, mockData.CompanyId, mockData.DataImportEventId, null, mockData.domainName, mockData.path)
+            .listDataImportEventDetails(
+                mockData.devTenantId,
+                mockData.CompanyId,
+                mockData.DataImportEventId,
+                null,
+                mockData.domainName,
+                mockData.path,
+            )
             .then((response) => {
                 expect(response).toBeInstanceOf(PaginatedResult);
                 expect(response.results.length).toBe(mockData.dataImportEventDetailsResponse.results.length);
                 expect(response.results).toEqual(mockData.dataImportEventDetailsResponse.results);
             });
+    });
+
+    test('returns undefined when the company has no import event detail', async () => {
+        (utilService as any).invokeInternalService = jest.fn((transaction, payload) => {
+            if (payload.queryName === 'listDataImportEventDetail') {
+                return Promise.resolve(mockData.dataImportEventDetailsEmpty);
+            }
+        });
+
+        await employeeImportService
+            .listDataImportEventDetails(
+                mockData.devTenantId,
+                mockData.CompanyId,
+                mockData.DataImportEventId,
+                null,
+                mockData.domainName,
+                mockData.path,
+            )
+            .then((response) => {
+                expect(response).toBe(undefined);
+            });
+    });
+});
+
+describe('employeeImport.Service.getTemplate', () => {
+    beforeEach(() => {
+        setup();
+    });
+
+    test('returns undefined if data import type is wrong', async () => {
+        (utilService as any).invokeInternalService = jest.fn((transaction, payload) => {
+            if (payload.queryName === 'getDataImportTypeById') {
+                return Promise.resolve(mockData.queryReturnedEmpty);
+            }
+        });
+
+        await employeeImportService.getTemplate(mockData.devTenantId, mockData.DataImportTypeId).then((response) => {
+            expect(response).toBe(undefined);
+        });
+    });
+});
+
+describe('employeeImport.Service.downloadImportData', () => {
+    beforeEach(() => {
+        setup();
+    });
+
+    test('returns undefined if data import type is wrong', async () => {
+        (utilService as any).invokeInternalService = jest.fn((transaction, payload) => {
+            if (payload.queryName === 'getImportTypeAndImportedFilePathByImportEventID') {
+                return Promise.resolve(mockData.queryReturnedEmpty);
+            }
+        });
+
+        await employeeImportService
+            .downloadImportData(
+                mockData.devTenantId,
+                mockData.CompanyId,
+                mockData.DataImportEventId,
+                null,
+                mockData.domainName,
+                mockData.path,
+            )
+            .then((response) => {
+                expect(response).toBe(undefined);
+            });
+    });
+});
+
+describe('employeeImport.Service.updateEmployee', () => {
+    beforeEach(() => {
+        setup();
+    });
+
+    test('should return undefined if accessToken parameter is empty', async () => {
+        await employeeImportService
+            .updateEmployee(null, 1, mockData.devTenantId, mockData.CompanyId, mockData.DataImportTypeId, mockData.DataImportEventId, '')
+            .then((response) => {
+                expect(response).toBe(undefined);
+            });
+    });
+});
+
+describe('employeeImport.Service.updateCompensation', () => {
+    beforeEach(() => {
+        setup();
+    });
+
+    test('should return undefined if accessToken parameter is empty', async () => {
+        await employeeImportService
+            .updateCompensation(
+                null,
+                1,
+                mockData.devTenantId,
+                mockData.CompanyId,
+                mockData.DataImportTypeId,
+                mockData.DataImportEventId,
+                '',
+            )
+            .then((response) => {
+                expect(response).toBe(undefined);
+            });
+    });
+});
+
+describe('employeeImport.Service.updateAlternateRate', () => {
+    beforeEach(() => {
+        setup();
+    });
+
+    test('should return undefined if accessToken parameter is empty', async () => {
+        await employeeImportService
+            .updateAlternateRate(
+                null,
+                1,
+                mockData.devTenantId,
+                mockData.CompanyId,
+                mockData.DataImportTypeId,
+                mockData.DataImportEventId,
+                '',
+            )
+            .then((response) => {
+                expect(response).toBe(undefined);
+            });
+    });
+});
+
+describe('employeeImport.Service.uploadUrl', () => {
+    beforeEach(() => {
+        setup();
+    });
+
+    test('should return error cause filename is empty', async () => {
+        await employeeImportService.uploadUrl(mockData.devTenantId, mockData.CompanyId, '').catch((error) => {
+            expect(error).toBeInstanceOf(ErrorMessage);
+            expect(error.message).toEqual('The parameter fileName is required');
+        });
     });
 });
--- services/api/employee-import/unit-tests/mock-data/mock-data.ts
+++ services/api/employee-import/unit-tests/mock-data/mock-data.ts
@@ -3,195 +3,283 @@ export const devTenantId = 'e5a19feb-0fca-4b61-9b7a-43a2a6dd7414';
 export const validId = '72';
 export const CompanyId = '1';
 export const DataImportEventId = '1';
+export const DataImportTypeId = '2';
 export const domainName = 'https://test.evolution-software.com';
 export const path = '/identity/tenants/1234/companies/1/employees/123/direct-deposits';
 
+export const dataImportTypesEmpty = {
+    recordsets: [[]],
+    recordset: [],
+    output: {},
+    rowsAffected: [0],
+};
+
+export const dataImportsEmpty = {
+    recordsets: [[{ totalCount: 0 }], []],
+    recordset: [{ totalCount: 0 }],
+    output: {},
+    rowsAffected: [0, 0, 0],
+};
+
+export const dataImportEventDetailsEmpty = {
+    recordsets: [[{ totalCount: 0 }], []],
+    recordset: [{ totalCount: 0 }],
+    output: {},
+    rowsAffected: [0, 0, 0],
+};
+
+export const queryReturnedEmpty = {
+    recordsets: [[]],
+    recordset: [],
+    output: {},
+    rowsAffected: [0],
+};
+
 export const dataImportTypes = {
-  recordsets: [ [ [Object], [Object] ] ],
-  recordset: [
-    {
-      ID: '1',
-      Name: 'Employee update1',
-      Description: 'Employee update Desc',
-      ImportProcess: 'UPDATE',
-      LastProgramEvent: ''
-    },
-    {
-      ID: '2',
-      Name: 'Alternate Rate update',
-      Description: 'Alternate Rate Desc',
-      ImportProcess: 'UPDATE',
-      LastProgramEvent: ''
-    }
-  ],
-  output: {},
-  rowsAffected: [ 2 ]
+    recordsets: [[[Object], [Object]]],
+    recordset: [
+        {
+            ID: '1',
+            Name: 'Employee update1',
+            Description: 'Employee update Desc',
+            ImportProcess: 'UPDATE',
+            LastProgramEvent: '',
+        },
+        {
+            ID: '2',
+            Name: 'Alternate Rate update',
+            Description: 'Alternate Rate Desc',
+            ImportProcess: 'UPDATE',
+            LastProgramEvent: '',
+        },
+    ],
+    output: {},
+    rowsAffected: [2],
 };
 
 export const dataImportTypeResponse = {
-    recordsets: [ [ [Object], [Object] ] ],
+    recordsets: [[[Object], [Object]]],
     recordset: [
-      {
-        id: '1',
-        name: 'Employee update1',
-        description: 'Employee update Desc',
-        importProcess: 'UPDATE',
-        lastProgramEvent: ''
-      },
-      {
-        id: '2',
-        name: 'Alternate Rate update',
-        description: 'Alternate Rate Desc',
-        importProcess: 'UPDATE',
-        lastProgramEvent: ''
-      }
+        {
+            id: '1',
+            name: 'Employee update1',
+            description: 'Employee update Desc',
+            importProcess: 'UPDATE',
+            lastProgramEvent: '',
+        },
+        {
+            id: '2',
+            name: 'Alternate Rate update',
+            description: 'Alternate Rate Desc',
+            importProcess: 'UPDATE',
+            lastProgramEvent: '',
+        },
     ],
     output: {},
-    rowsAffected: [ 2 ]
-  };
-  
+    rowsAffected: [2],
+};
+
 export const dataImports = {
-  recordsets: [
-      [{ totalCount: 3 }],
-      [
-          {
-              ID: "47",
-              CompanyID: "1",
-              DataImportTypeID: "1",
-              Status: "Completed",
-              LastProgramEvent: "",
-              CreationDate: "2023-07-17T00:00:00.000Z",
-              LastUpdatedDate: "2023-06-01T00:00:00.000Z",
-              LastUserID: "1"
-          },
-          {
-              ID: "46",
-              CompanyID: "1",
-              DataImportTypeID: "1",
-              Status: "Completed",
-              LastProgramEvent: "",
-              CreationDate: "2023-07-16T00:00:00.000Z",
-              LastUpdatedDate: "2023-06-01T00:00:00.000Z",
-              LastUserID: "1"
-          },
-          {
-              ID: "45",
-              CompanyID: "1",
-              DataImportTypeID: "1",
-              Status: "Completed",
-              LastProgramEvent: "",
-              CreationDate: "2023-07-15T00:00:00.000Z",
-              LastUpdatedDate: "2023-06-01T00:00:00.000Z",
-              LastUserID: "1"
-          },
-      ],
-  ],
-  recordset: [{ totalCount: 3 }],
-  output: {},
-  rowsAffected: [1, 1, 3],
+    recordsets: [
+        [{ totalCount: 3 }],
+        [
+            {
+                ID: '47',
+                CompanyID: '1',
+                DataImportTypeID: '1',
+                Status: 'Completed',
+                LastProgramEvent: '',
+                CreationDate: '2023-07-17T00:00:00.000Z',
+                LastUpdatedDate: '2023-06-01T00:00:00.000Z',
+                LastUserID: '1',
+            },
+            {
+                ID: '46',
+                CompanyID: '1',
+                DataImportTypeID: '1',
+                Status: 'Completed',
+                LastProgramEvent: '',
+                CreationDate: '2023-07-16T00:00:00.000Z',
+                LastUpdatedDate: '2023-06-01T00:00:00.000Z',
+                LastUserID: '1',
+            },
+            {
+                ID: '45',
+                CompanyID: '1',
+                DataImportTypeID: '1',
+                Status: 'Completed',
+                LastProgramEvent: '',
+                CreationDate: '2023-07-15T00:00:00.000Z',
+                LastUpdatedDate: '2023-06-01T00:00:00.000Z',
+                LastUserID: '1',
+            },
+        ],
+    ],
+    recordset: [{ totalCount: 3 }],
+    output: {},
+    rowsAffected: [1, 1, 3],
+};
+
+export const dataImportsByDataImportTypeId = {
+    recordsets: [
+        [{ totalCount: 3 }],
+        [
+            {
+                ID: '47',
+                CompanyID: '1',
+                DataImportTypeID: '2',
+                Status: 'Completed',
+                LastProgramEvent: '',
+                CreationDate: '2023-07-17T00:00:00.000Z',
+                LastUpdatedDate: '2023-06-01T00:00:00.000Z',
+                LastUserID: '1',
+            },
+            {
+                ID: '45',
+                CompanyID: '1',
+                DataImportTypeID: '2',
+                Status: 'Completed',
+                LastProgramEvent: '',
+                CreationDate: '2023-07-15T00:00:00.000Z',
+                LastUpdatedDate: '2023-06-01T00:00:00.000Z',
+                LastUserID: '1',
+            },
+        ],
+    ],
+    recordset: [{ totalCount: 3 }],
+    output: {},
+    rowsAffected: [1, 1, 3],
 };
 
 export const dataImportsResponse = {
-  limit: 30,
-  count: 3,
-  totalCount: 3,
-  results: [
-      {
-          id: "47",
-          companyId: "1",
-          dataImportTypeId: "1",
-          status: "Completed",
-          lastProgramEvent: "",
-          creationDate: "2023-07-17T00:00:00.000Z",
-          lastUpdatedDate: "2023-06-01T00:00:00.000Z",
-          lastUserId: "1"
-      },
-      {
-          id: "46",
-          companyId: "1",
-          dataImportTypeId: "1",
-          status: "Completed",
-          lastProgramEvent: "",
-          creationDate: "2023-07-16T00:00:00.000Z",
-          lastUpdatedDate: "2023-06-01T00:00:00.000Z",
-          lastUserId: "1"
-      },
-      {
-          id: "45",
-          companyId: "1",
-          dataImportTypeId: "1",
-          status: "Completed",
-          lastProgramEvent: "",
-          creationDate: "2023-07-15T00:00:00.000Z",
-          lastUpdatedDate: "2023-06-01T00:00:00.000Z",
-          lastUserId: "1"
-      },
-  ],
+    limit: 30,
+    count: 3,
+    totalCount: 3,
+    results: [
+        {
+            id: '47',
+            companyId: '1',
+            dataImportTypeId: '1',
+            status: 'Completed',
+            lastProgramEvent: '',
+            creationDate: '2023-07-17T00:00:00.000Z',
+            lastUpdatedDate: '2023-06-01T00:00:00.000Z',
+            lastUserId: '1',
+        },
+        {
+            id: '46',
+            companyId: '1',
+            dataImportTypeId: '1',
+            status: 'Completed',
+            lastProgramEvent: '',
+            creationDate: '2023-07-16T00:00:00.000Z',
+            lastUpdatedDate: '2023-06-01T00:00:00.000Z',
+            lastUserId: '1',
+        },
+        {
+            id: '45',
+            companyId: '1',
+            dataImportTypeId: '1',
+            status: 'Completed',
+            lastProgramEvent: '',
+            creationDate: '2023-07-15T00:00:00.000Z',
+            lastUpdatedDate: '2023-06-01T00:00:00.000Z',
+            lastUserId: '1',
+        },
+    ],
+};
+
+export const dataImportsByDataImportTypeResponse = {
+    limit: 30,
+    count: 3,
+    totalCount: 3,
+    results: [
+        {
+            id: '47',
+            companyId: '1',
+            dataImportTypeId: '2',
+            status: 'Completed',
+            lastProgramEvent: '',
+            creationDate: '2023-07-17T00:00:00.000Z',
+            lastUpdatedDate: '2023-06-01T00:00:00.000Z',
+            lastUserId: '1',
+        },
+        {
+            id: '45',
+            companyId: '1',
+            dataImportTypeId: '2',
+            status: 'Completed',
+            lastProgramEvent: '',
+            creationDate: '2023-07-15T00:00:00.000Z',
+            lastUpdatedDate: '2023-06-01T00:00:00.000Z',
+            lastUserId: '1',
+        },
+    ],
 };
 
 export const dataImportEventDetails = {
-  recordsets: [
-      [{ totalCount: 3 }],
-      [
-          {
-              ID: "47",
-              DataImportEventID: "1",
-              CSVRowStatus: "Ready",
-              CSVRowNumber: "1",
-              CSVRowNotes: "Notes 1",
-              CSVRowData: "xxx",
-              LastUserID: "1",
-              LastProgramEvent: "",
-              CreationDate: "2023-07-17T00:00:00.000Z",
-              LastUpdatedDate: "2023-06-01T00:00:00.000Z",
-          },
-          {
-            ID: "48",
-            DataImportEventID: "1",
-            CSVRowStatus: "Ready",
-            CSVRowNumber: "2",
-            CSVRowNotes: "Notes 2",
-            CSVRowData: "yyy",
-            LastUserID: "1",
-            LastProgramEvent: "",
-            CreationDate: "2023-07-17T00:00:00.000Z",
-            LastUpdatedDate: "2023-06-01T00:00:00.000Z",
-          },
-      ],
-  ],
-  recordset: [{ totalCount: 3 }],
-  output: {},
-  rowsAffected: [1, 1, 3],
+    recordsets: [
+        [{ totalCount: 3 }],
+        [
+            {
+                ID: '47',
+                DataImportEventID: '1',
+                CSVRowStatus: 'Ready',
+                CSVRowNumber: '1',
+                CSVRowNotes: 'Notes 1',
+                CSVRowData: 'xxx',
+                LastUserID: '1',
+                LastProgramEvent: '',
+                CreationDate: '2023-07-17T00:00:00.000Z',
+                LastUpdatedDate: '2023-06-01T00:00:00.000Z',
+            },
+            {
+                ID: '48',
+                DataImportEventID: '1',
+                CSVRowStatus: 'Ready',
+                CSVRowNumber: '2',
+                CSVRowNotes: 'Notes 2',
+                CSVRowData: 'yyy',
+                LastUserID: '1',
+                LastProgramEvent: '',
+                CreationDate: '2023-07-17T00:00:00.000Z',
+                LastUpdatedDate: '2023-06-01T00:00:00.000Z',
+            },
+        ],
+    ],
+    recordset: [{ totalCount: 3 }],
+    output: {},
+    rowsAffected: [1, 1, 3],
 };
 
 export const dataImportEventDetailsResponse = {
-  limit: 30,
-  count: 3,
-  totalCount: 3,
-  results: [
-      {
-          id: "47",
-          dataImportEventId: "1",
-          csvRowStatus: "Ready",
-          csvRowNumber: "1",
-          csvRowNotes: "Notes 1",
-          csvRowData: "xxx",
-          lastUserId: "1",
-          lastProgramEvent: "",
-          creationDate: "2023-07-17T00:00:00.000Z",
-          lastUpdatedDate: "2023-06-01T00:00:00.000Z",
-      },
-      {
-        id: "48",
-        dataImportEventId: "1",
-        csvRowStatus: "Ready",
-        csvRowNumber: "2",
-        csvRowNotes: "Notes 2",
-        csvRowData: "yyy",
-        lastUserId: "1",
-        lastProgramEvent: "",
-        creationDate: "2023-07-17T00:00:00.000Z",
-        lastUpdatedDate: "2023-06-01T00:00:00.000Z",
-      },
-  ],
+    limit: 30,
+    count: 3,
+    totalCount: 3,
+    results: [
+        {
+            id: '47',
+            dataImportEventId: '1',
+            csvRowStatus: 'Ready',
+            csvRowNumber: '1',
+            csvRowNotes: 'Notes 1',
+            csvRowData: 'xxx',
+            lastUserId: '1',
+            lastProgramEvent: '',
+            creationDate: '2023-07-17T00:00:00.000Z',
+            lastUpdatedDate: '2023-06-01T00:00:00.000Z',
+        },
+        {
+            id: '48',
+            dataImportEventId: '1',
+            csvRowStatus: 'Ready',
+            csvRowNumber: '2',
+            csvRowNotes: 'Notes 2',
+            csvRowData: 'yyy',
+            lastUserId: '1',
+            lastProgramEvent: '',
+            creationDate: '2023-07-17T00:00:00.000Z',
+            lastUpdatedDate: '2023-06-01T00:00:00.000Z',
+        },
+    ],
 };
