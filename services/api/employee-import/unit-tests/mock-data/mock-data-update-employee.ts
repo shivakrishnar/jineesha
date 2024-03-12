e51b0c341a34293344290038f9188cd9999dbfe0
johnn <johnnathan.almeida.ctr@asuresoftware.com>
2024-03-12 10:32:00 -0300

    fix: changing from address email in dev and staging config files

--- services/api/employee-import/unit-tests/mock-data/mock-data-update-employee.ts
+++ services/api/employee-import/unit-tests/mock-data/mock-data-update-employee.ts
@@ -39,7 +39,7 @@ export const resultCSVHeader = {
     recordsets: [[{ totalCount: 1 }]],
     recordset: [
         {
-            CSVHeader: 'Employee Code,Birthdate,Time Clock Number,Email,Home Phone,Work Phone,Cell Phone,Gender,Ethnicity,Education Level,Tobacco User,Disabled,Military Reserve,Veteran,Memo 1,Memo 2,Memo 3,Pay Frequency,Standard Payroll Hours,FLSA Classification,Position,Reports To 1,Reports To 2,Reports To 3,Supervisor (SC),Benefit Class/Eligibility Group,EEO Category,Worker Comp Code,Change Reason,Comment'
+            CSVHeader: 'Employee Identifier,Birthdate,Time Clock Number,Email,Home Phone,Work Phone,Cell Phone,Gender,Ethnicity,Education Level,Tobacco User,Disabled,Military Reserve,Veteran,Memo 1,Memo 2,Memo 3,Pay Frequency,Standard Payroll Hours,FLSA Classification,Position,Reports To 1,Reports To 2,Reports To 3,Supervisor (SC),Benefit Class/Eligibility Group,EEO Category,Worker Comp Code,Change Reason,Comment'
         }
     ],
     output: {},
@@ -75,6 +75,45 @@ export const resultInfoEmployeeAHREVO = {
             EvoEmployeeId: '123',
             EvoCompanyId: '123',
             EvoClientId: '123'
+        }
+    ],
+    output: {},
+    rowsAffected: [1],
+}
+
+export const resultAllFieldsForUpdateEmployee = {
+    recordsets: [[{ totalCount: 1 }]],
+    recordset: [
+        {
+            BirthDate: '4/22/2020',
+            ClockNumber: '123',
+            Email: 'test@test.com',
+            PhoneHome: '',
+            PhoneWork: '',
+            PhoneCell: '',
+            Gender: 'M',
+            Ethnicity: '',
+            Education: '',
+            TobaccoUser: '0',
+            IsDisabled: '0',
+            MilitaryReserve: '',
+            Veteran: '',
+            Memo1: '1',
+            Memo2: '2',
+            Memo3: '3',
+            PayFrequency: 'Monthly',
+            StandardPayrollHours: '10', 
+            FLSAClassification: '',
+            Position: '',
+            ReportsTo1: '10',
+            ReportsTo2: '12',
+            ReportsTo3: '13',
+            SupervisorSC: 'Ka',
+            BenefitClass: '',
+            EEOCategory: '',
+            WorkerCompCode: '2300(VT)',
+            ChangeReason: '',
+            Comment: 'Test'
         }
     ],
     output: {},
