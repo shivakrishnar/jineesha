af9d450231004005e239e49ba46a37fe8fd04aee
Marden Rodrigues <marden.rodrigues.ctr@asuresoftware.com>
2024-05-14 10:55:03 -0300

    feat: MJ-15817 - Change the JobPosting  GET by company endpoint to allow filter by multiple companies

--- services/queries/applicant-tracking/getJobPostingByCompany.sql
+++ services/queries/applicant-tracking/getJobPostingByCompany.sql
@@ -1,28 +1,35 @@
-declare @_companyid bigint = @CompanyID
+declare @_companyId nvarchar(max) = @CompanyID
 
-select count(*) as totalCount
-from ATJobPosting
-where CompanyID = @_companyid
+select 
+	count(*) as totalCount
+from 
+	ATJobPosting
+where 
+	CompanyID in(select * from dbo.SplitString(@_companyId, '-'))
 
-select jp.ID as id,
-	   jp.CompanyID as companyId,
-	   c.CompanyName as companyName,
-	   jp.ATApplicationVersionID as aTApplicationVersionId,
-	   jp.PositionTypeID as positionTypeId,
-	   jp.OrganizationType1ID as organizationType1Id,
-	   jp.OrganizationType2ID as organizationType2Id,
-	   jp.OrganizationType3ID as organizationType3Id,
-	   jp.OrganizationType4ID as organizationType4Id,
-	   jp.OrganizationType5ID as organizationType5Id,
-	   jp.WorkerCompTypeID as workerCompTypeId,
-	   jp.Title as title,
-	   jp.Description as description,
-	   jp.LinkKey as linkKey,
-	   jp.IsOpen as isOpen,
-	   jp.JazzHrPositionOpeningID as jazzHrPositionOpeningId
-
-from ATJobPosting jp
-		inner join Company c on c.id = jp.CompanyID
-        
-where jp.CompanyID = @_companyid
-order by jp.Title
+select 
+	jp.ID as id,
+	jp.CompanyID as companyId,
+	c.CompanyName as companyName,
+	jp.ATApplicationVersionID as aTApplicationVersionId,
+	jp.PositionTypeID as positionTypeId,
+	jp.OrganizationType1ID as organizationType1Id,
+	jp.OrganizationType2ID as organizationType2Id,
+	jp.OrganizationType3ID as organizationType3Id,
+	jp.OrganizationType4ID as organizationType4Id,
+	jp.OrganizationType5ID as organizationType5Id,
+	jp.WorkerCompTypeID as workerCompTypeId,
+	jp.Title as title,
+	jp.Description as description,
+	jp.LinkKey as linkKey,
+	jp.IsOpen as isOpen,
+	jp.JazzHrPositionOpeningID as jazzHrPositionOpeningId
+from 
+	ATJobPosting jp inner join 
+	Company c on c.id = jp.CompanyID
+where 
+	jp.CompanyID in(select * from dbo.SplitString(@_companyId, '-'))
+order by
+	c.CompanyName,
+	jp.Title, 
+	jp.ID
